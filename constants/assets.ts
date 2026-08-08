c3c5d4e2be86a00cab241049a016104f8e10ec7f
GW <kowonp@gmail.com>
2026-08-09 00:10:43 +0900

    feat: API, Assets, Error messages 상수 추가

---
+++ constants/assets.ts
@@ -0,0 +1,3 @@
+export const ASSETS = {
+  DEFAULT_AVATAR: "/images/default_avatar.svg",
+};
---
+++ constants/error-messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  UNAUTHORIZED: "권한이 없습니다.",
+  NO_USER_FOUND: "사용자 목록 조회 실패",
+  NO_ORGANIZATION_FOUND: "조직 목록 조회 실패",
+  NO_WORKFLOW_GRAPH: "워크플로우 그래프 조회 실패",
+};
