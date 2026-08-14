5abc2a0ba5157aa5767d932bdad200781dbd918a
GW <kowonp@gmail.com>
2026-08-14 23:00:01 +0900

    feat: 워크플로우 실행 로직 개선 및 실행 취소 기능 추가

--- features/workflows/actions.ts
+++ features/workflows/actions.ts
@@ -1,19 +1,30 @@
 "use server";
 
+import { ERROR_MESSAGES } from "@/constants/error-messages";
 import { ROUTES } from "@/constants/routes";
-import { createWorkflow, deleteWorkflow } from "@/features/workflows/queries";
+import {
+  createWorkflow,
+  deleteWorkflow,
+  updateWorkflowGraph,
+} from "@/features/workflows/queries";
+import type { runWorkflowTask } from "@/features/workflows/tasks/run-workflow";
+import { WorkflowGraph } from "@/libs/db/schema";
 import { liveblocks } from "@/libs/liveblocks";
-import type { helloWorldTask } from "@/trigger/example";
 import { auth } from "@clerk/nextjs/server";
-import { tasks } from "@trigger.dev/sdk";
+import { runs, tasks } from "@trigger.dev/sdk";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+/**
+ * 워크플로우 생성
+ *
+ * @param name 워크플로우 이름
+ */
 export const createWorkflowAction = async (name: string) => {
   const { orgId } = await auth();
 
   if (!orgId) {
-    throw new Error("No organization found");
+    throw new Error(ERROR_MESSAGES.NO_ORGANIZATION_FOUND);
   }
 
   const createdWorkflow = await createWorkflow(name, orgId);
@@ -28,25 +39,16 @@ export const createWorkflowAction = async (name: string) => {
   redirect(ROUTES.WORKFLOWS.DETAIL(createdWorkflow.id));
 };
 
-export const runWorkflowAction = async (workflowId: string) => {
-  const { orgId } = await auth();
-
-  if (!orgId) {
-    throw new Error("No organization found");
-  }
-
-  const handle = await tasks.trigger<typeof helloWorldTask>("hello-world", {
-    message: `workflowId: ${workflowId}`,
-  });
-
-  return handle;
-};
-
+/**
+ * 워크플로우 삭제
+ *
+ * @param workflowId 워크플로우 ID
+ */
 export const deleteWorkflowAction = async (workflowId: string) => {
   const { orgId } = await auth();
 
   if (!orgId) {
-    throw new Error("No organization found");
+    throw new Error(ERROR_MESSAGES.NO_ORGANIZATION_FOUND);
   }
 
   await deleteWorkflow(workflowId, orgId);
@@ -54,4 +56,46 @@ export const deleteWorkflowAction = async (workflowId: string) => {
 
   revalidatePath(ROUTES.WORKFLOWS.INDEX, "layout");
   redirect(ROUTES.DASHBOARD);
+};
+
+/**
+ * 워크플로우 실행
+ *
+ * @param workflowId 워크플로우 ID
+ * @param graph 워크플로우 그래프
+ */
+export const runWorkflowAction = async (
+  workflowId: string,
+  graph: WorkflowGraph
+) => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    throw new Error(ERROR_MESSAGES.NO_ORGANIZATION_FOUND);
+  }
+
+  await updateWorkflowGraph({ id: workflowId, organizationId: orgId, graph });
+
+  const handle = await tasks.trigger<typeof runWorkflowTask>(
+    "run-workflow",
+    { workflowId, organizationId: orgId },
+    { tags: [`workflow:${workflowId}`] }
+  );
+
+  return handle;
+};
+
+/**
+ * 워크플로우 실행 취소
+ *
+ * @param runId 실행 ID
+ */
+export const cancelRunWorkflowAction = async (runId: string) => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    throw new Error(ERROR_MESSAGES.NO_ORGANIZATION_FOUND);
+  }
+
+  await runs.cancel(runId);
 };
