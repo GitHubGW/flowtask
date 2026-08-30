7f17b1d337e28992a13fdeb7ce18a17f559fdc34
GW <kowonp@gmail.com>
2026-08-31 02:24:37 +0900

    refactor: useIsMobile 훅을 useSyncExternalStore로 재작성

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,21 +1,30 @@
-import * as React from "react";
+import { useSyncExternalStore } from "react";
 
 const MOBILE_BREAKPOINT = 768;
+const MOBILE_MEDIA_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`;
+
+const subscribeToMobileQuery = (onStoreChange: () => void) => {
+  const mediaQueryList = window.matchMedia(MOBILE_MEDIA_QUERY);
+
+  mediaQueryList.addEventListener("change", onStoreChange);
+
+  return () => {
+    mediaQueryList.removeEventListener("change", onStoreChange);
+  };
+};
+
+const getMobileSnapshot = () => {
+  return window.matchMedia(MOBILE_MEDIA_QUERY).matches;
+};
+
+const getServerSnapshot = () => {
+  return false;
+};
 
 export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined
+  return useSyncExternalStore(
+    subscribeToMobileQuery,
+    getMobileSnapshot,
+    getServerSnapshot
   );
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
-  }, []);
-
-  return !!isMobile;
 };
