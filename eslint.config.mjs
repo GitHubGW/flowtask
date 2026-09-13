ab149c5a5b2ccdeda4add622fdc7e7b2c7301aa8
GW <kowonp@gmail.com>
2026-09-14 00:45:57 +0900

    chore: import 정렬을 위한 ESLint와 VS Code 환경 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: { "simple-import-sort/imports": "warn" },
+  },
   globalIgnores([
     ".next/**",
     "out/**",
     "build/**",
+    "storybook-static/**",
     "next-env.d.ts",
     ".trigger/**",
   ]),
